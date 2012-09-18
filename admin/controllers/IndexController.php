300612e3354a5c2bb63a001d9a436a5e864fc758
prg.pacheco <prg.pacheco@DELL.interbits.com.br>
2012-09-18 17:26:33 -0300

    Implementação de Componetes HTML (Select / Table) Grid de TOP 10

--- admin/controllers/IndexController.php
+++ admin/controllers/IndexController.php
@@ -32,9 +32,31 @@
                 $id_materia             = Request::post("id_materia", "NUMBER");
                 $id_fonte_vestibular    = Request::post("id_fonte_vestibular", "NUMBER");
                 
-                $objView->COMBO_MATERIAS    = HtmlComponent::select($m_top10->getMateriasSelectBox(), "id_materia", "Selecione uma matéria", null, $id_materia);
-                $objView->COMBO_FONTES      = HtmlComponent::select($m_top10->getFontesSelectBox(), "id_fonte_vestibular", "Selecione uma fonte", null, $id_fonte_vestibular, null, null, true);
-                $objView->TB_QUESTOES       = HtmlComponent::table($m_top10->listaQuestoesTop10($id_materia, $id_fonte_vestibular), 'table_questoes', 'table_top10');
+                //Opções do <select> de matérias
+                $cbo_materias_opts                  = new \stdClass();
+                $cbo_materias_opts->id              = "id_materia";
+                $cbo_materias_opts->first_option    = "Selecione uma matéria";
+                $cbo_materias_opts->select_option   = $id_materia;
+                
+                $objView->COMBO_MATERIAS = HtmlComponent::select($m_top10->getMateriasSelectBox(), $cbo_materias_opts);
+                
+                //Opções do <select> de fontes
+                $cbo_fontes_opts                  = new \stdClass();
+                $cbo_fontes_opts->id              = "id_fonte_vestibular";
+                $cbo_fontes_opts->first_option    = "Selecione uma fonte";
+                $cbo_fontes_opts->select_option   = $id_fonte_vestibular;
+                $cbo_fontes_opts->disabled        = true;
+                
+                $objView->COMBO_FONTES = HtmlComponent::select($m_top10->getFontesSelectBox(), $cbo_fontes_opts);
+                
+                //Opções do <table> de questões
+                $tb_questos_opts                  = new \stdClass();
+                $tb_questos_opts->id              = "table_questoes";
+                $tb_questos_opts->disabled        = true;
+                $tb_questos_opts->class           = "table_questoes";
+                $tb_questos_opts->html_template   = "table_top10";
+                
+                $objView->TB_QUESTOES = HtmlComponent::table($m_top10->listaQuestoesTop10($id_materia, $id_fonte_vestibular), $tb_questos_opts);
                 
                 $objView->render();            
             }catch(Exception $e){
--- sys/classes/comps/HtmlComponent.php
+++ sys/classes/comps/HtmlComponent.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ * Classe para gerarmos elementos HTML que precisam interagir com PHP
+ * Arquivos baseados em PHTML
+ * 
+ * @property string $html_path Caminho dos templates HTML padrões
+ * @property string $html_path_templates Caminho dos templates HTML personalizados
+ * @property string $default_html Arquivo HTML padrão a ser utilizado quando gerar um elemento
+ * 
+ * @property array $table_data Array de dados para criação de uma <table> HTML
+ * @property array $sel_options Array de dados para criação das <options> de um elemento <select>
+ * @property string $first_option Primeiro elemento de um <select>
+ * @property string $select_option Valor do elemento que deve ser selecionado na criação do <select>
+
+ * @property string $id Valor da propriedade ID do elemento HTML a ser criado
+ * @property string $onchange Valor da propriedade ONCHANGE do elemento HTML a ser criado
+ * @property string $class Valor propriedade CLASS (css) do elemento HTML a ser criado
+ * @property string $style Valor da propriedade STYLE (css) do elemento HTML a ser criado
+ * @property boolean $disabled Valor propriedade DISABLE do elemento HTML a ser criado
+ * 
+ * @author Interbis <interbist@interbits.com.br>
+ */
 class HtmlComponent {
     static $html_path           = "/sys/classes/comps/html/";
     static $html_path_templates = "/comps/html/";
@@ -8,31 +29,58 @@ class HtmlComponent {
     
     private static $sel_options;
     private static $select_option;
+    private static $first_option;
     
     private static $id;
-    private static $first_option;
     private static $onchange;
     private static $class;
     private static $style;
-    private static $disabled;
+    private static $disabled = FALSE;
     
-    public static function select($arr_options, $id, $first_option = null, $onchange = null, $select_option = 0, $class = null, $style = null, $disabled = FALSE, $html_template = null){
+    /**
+     * Cria um elemento HTML <select> e seus <option>
+     * 
+     * @param array $arr_options Dados a serem carregados nos <option>
+     * <code>
+     * $arr_options = array( 
+     *                      array(
+     *                              "ID" => 1, 
+     *                              "TEXT" => "Biologia"
+     *                      ), 
+     *                      array(
+     *                              "ID" => 2, 
+     *                              "TEXT" => "Física"
+     *                      ) 
+     *                );
+     * </code>
+     * 
+     * @param stdClass $opts Objeto utilizado para iniciar as propriedades que você deseja usar no elemento HTML
+     * <code>
+     * $opts = new stdClass();
+     * 
+     * $opts->id     = "id_do_elemento";
+     * $opts->class  = "classe_1 classe_2";
+     * </code>
+     * 
+     * @return string $html Retorna o HTML gerado para o elemento <select>
+     */
+    public static function select($arr_options, $opts){
         try{
             //Setando propriedades
             self::$sel_options      = $arr_options;
-            self::$id               = $id;
-            self::$first_option     = $first_option;
-            self::$onchange         = $onchange;
-            self::$select_option    = $select_option;
-            self::$class            = $class;
-            self::$style            = $style;
-            self::$disabled         = $disabled;
+            self::$id               = @$opts->id;
+            self::$first_option     = @$opts->first_option;
+            self::$onchange         = @$opts->onchange;
+            self::$select_option    = @$opts->select_option;
+            self::$class            = @$opts->class;
+            self::$style            = @$opts->style;
+            self::$disabled         = @$opts->disabled;
             
             //Setando template default
             self::$default_html = 'select';
             
             //Renderizando o HTML
-            return self::renderHtml($html_template);
+            return self::renderHtml(@$opts->html_template);
         }catch(Exception $e){
             echo ">>>>>>>>>>>>>>> Erro Fatal - HtmlComponent <<<<<<<<<<<<<<< <br />\n";
             echo "Erro: " . $e->getMessage() . "<br />\n";
@@ -43,17 +91,31 @@ class HtmlComponent {
         }
     }
     
-    public static function table($arr_data, $class = null, $html_template = null){
+    /**
+     * Cria um elemento HTML <table>
+     * 
+     * @param array $arr_data Dados para geração de linhas e colunas
+     * @param stdClass $opts Objeto utilizado para iniciar as propriedades que você deseja usar no elemento HTML
+     * <code>
+     * $opts = new stdClass();
+     * 
+     * $opts->id     = "id_do_elemento";
+     * $opts->class  = "classe_1 classe_2";
+     * </code>
+     * 
+     * @return string Retorna o HTML gerado para o elemento <table>
+     */
+    public static function table($arr_data, $opts){
         try{
             //Setando propriedades
             self::$table_data   = $arr_data;
-            self::$class        = $class;
+            self::$class        = @$opts->class;
             
             //Setando template default
             self::$default_html = 'table';
             
             //Renderizando o HTML
-            return self::renderHtml($html_template);
+            return self::renderHtml(@$opts->html_template);
         }catch(Exception $e){
             echo ">>>>>>>>>>>>>>> Erro Fatal - HtmlComponent <<<<<<<<<<<<<<< <br />\n";
             echo "Erro: " . $e->getMessage() . "<br />\n";
@@ -64,6 +126,12 @@ class HtmlComponent {
         }
     }
     
+    /**
+     * 
+     * @param string $html_template Nome do arquivo físico a ser processado (PHTML)
+     * 
+     * @return string HTML porcessado
+     */
     private static function renderHtml($html_template){
         try{
             if($html_template != null){
