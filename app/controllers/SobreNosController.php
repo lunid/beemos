0293dabf127a263250a956f1f660b85d630d20af
prg.pacheco <prg.pacheco@DELL.interbits.com.br>
2012-09-26 15:09:01 -0300

    Ajustes

--- app/controllers/SobreNosController.php
+++ app/controllers/SobreNosController.php
@@ -3,25 +3,24 @@
     use \sys\classes\mvc\Controller;    
     use \sys\classes\mvc\View;        
     use \sys\classes\mvc\ViewPart;        
-    use \app\models\HomeModel;       
     
     /**
     * Classe Controller usada com default quando nenhuma outra é informada.
     * Refere-se à página inicial da raíz do site.
     */
     class SobreNos extends Controller {
-
         /**
-        *Conteúdo da página Sobre Nós
+        *   Conteúdo da página Sobre Nós
         */
         function actionIndex(){
 	    $this->actionAinterbits();
         }      
         
         function actionAinterbits(){          
-            $objPartPg              = new ViewPart('blank');            
             $objPartLayout          = new ViewPart('templates/navegacaoVertical');
             $objPartLayout->IMG     = "<img src='app/views/images/testeira.jpg'>";
+            
+            $objPartPg              = new ViewPart('sobreNos_aInterbits');            
             $objPartLayout->BODY    = $objPartPg->render();                                    
             
             $objView           = new View($objPartLayout);            
