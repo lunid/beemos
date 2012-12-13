a23b5eb8c9b6ccdae59879885e1ff70c6beed4a2
prg.pacheco <prg.pacheco@DELL.interbits.com.br>
2012-12-13 15:24:04 -0200

    Área de Escola - Usuários - Modal de Cadastro Aplicação de CSS

---
+++ admin/classes/controllers/CriacaoController.php
@@ -0,0 +1,39 @@
+<?php
+    use \admin\classes\controllers\AdminController;
+    
+    class Criacao extends AdminController{
+        function actionIndex(){
+            //View do Grid de Escolas
+            $objViewPart = $this->mkViewPart('criacao/conteudo');
+                
+            //Template
+            $tpl                = $this->mkView();
+            $tpl->setLayout($objViewPart);
+            $tpl->TITLE         = 'ADM | Criação';
+
+            //Instância de JS
+            $tpl->setJs('criacao/javascript');
+            $tpl->setCss('criacao/estilo');
+            $tpl->forceCssJsMinifyOn();
+            
+            $tpl->render('escola');
+        }
+        
+        function actionAbas(){
+            //View do Grid de Escolas
+            $objViewPart = $this->mkViewPart('criacao/aba');
+                
+            //Template
+            $tpl                = $this->mkView();
+            $tpl->setLayout($objViewPart);
+            $tpl->TITLE         = 'ADM | Criação';
+
+            //Instância de JS
+            $tpl->setJs('criacao/javascript');
+            $tpl->setCss('criacao/estilo');
+            $tpl->forceCssJsMinifyOn();
+            
+            $tpl->render('escola');
+        }
+    }
+?>
