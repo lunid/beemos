2e74151f0548f617094396ca8f17e16b8a046233
prg.pacheco <prg.pacheco@DELL.interbits.com.br>
2012-12-20 09:42:23 -0200

    Ajuste de Git Ignore

--- assets/js/app/_home_min.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".pix_diapo").diapo()});
--- assets/js/util/_dictionary.js
+++
@@ -1,1 +0,0 @@
-xml_dic=null;Dic=function(){};Dic.loadMsg=function(class_name,msg_param,method){try{if($.trim(method)==""||method==null){method="default"}var xml_class=$(xml_dic).find(class_name);var xml_method=$(xml_class).find(method);var msg=xml_method.find('msg[id="'+msg_param+'"]');return msg.text()}catch(err){alert("Error to load message in dictionary: "+err.message)}};
