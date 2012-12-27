a4943c82ccc7906a3b99eb423dcc9497ce829c47
prg.pacheco <prg.pacheco@DELL.interbits.com.br>
2012-12-27 11:35:50 -0200

    Formulário de Contato Padronização de aguarde

--- assets/js/app/home.js
+++ assets/js/app/home.js
@@ -1,4 +1,8 @@
 //Banner rotativo
 $(document).ready(function() {
     $('.pix_diapo').diapo();
+    
+    //Formulário experimente
+    formExperimente = new Form();
+    formExperimente.init("form_experimente");
 });
