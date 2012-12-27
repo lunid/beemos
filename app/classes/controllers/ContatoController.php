65c7563c130b9f6a0641d8d7b6292d47402d00db
prg.pacheco <prg.pacheco@DELL.interbits.com.br>
2012-12-27 09:50:26 -0200

    Formulário de Contato Padronização de aguarde

--- app/classes/controllers/ContatoController.php
+++ app/classes/controllers/ContatoController.php
@@ -3,6 +3,8 @@
     use \sys\classes\mvc\Controller;        
     use \sys\classes\mvc\ViewPart;
     use \app\classes\views\ViewSite;
+    use \sys\classes\util\Request;
+    use \sys\classes\util\Component;
     
     class Contato extends Controller {
         /**
@@ -31,6 +33,68 @@
                 echo "Linha:  " . $e->getLine() . "<br />\n";
                 echo "<br />\n";
                 die;
+            }
+        }
+        
+        public function actionEnviar(){
+            try{
+                //Objeto de retorno
+                $ret            = new stdClass();
+                $ret->status    = false;
+                $ret->msg       = "Falha ao enviar mensagem! Tente mais tarde.";
+                
+                //Recebe valores
+                $nome       = Request::post("nome");
+                $email      = Request::post("email");
+                $assunto    = Request::post("assunto", "NUMBER");
+                $msg        = Request::post("msg");
+                
+                //Define destino
+                switch ($assunto){
+                    case 1:
+                        $para       = "duvidas@interbits.com.br";
+                        $assuntoTxt = "[Contato Site] Dúvidas";
+                        break;
+                    case 2:
+                        $para       = "suporte@interbits.com.br";
+                        $assuntoTxt = "[Contato Site] Suporte Técnico";
+                        break;
+                    case 3:
+                        $para       = "cobranca@interbits.com.br";
+                        $assuntoTxt = "[Contato Site] Cobrança / Financeiro";
+                        break;
+                    default :
+                        $para       = "interbits@interbits.com.br";
+                        $assuntoTxt = "[Contato Site] Dúvidas";
+                        break;
+                }
+                
+                $para = "mpcbarone@gmail.com";
+                
+                //Componente para disparo de e-mail
+                $objMail = Component::mail();
+                
+                $objMail->setFrom($email, $nome);
+                $objMail->addAddress($para);
+                $objMail->setSubject($assuntoTxt);
+                
+                if (!$objMail->send()){
+                    $ret->msg = "Falha ao disparar e-mail! Tente mais tarde.";
+                    echo json_encode($ret);
+                }else{
+                    //Retorno OK
+                    $ret->status    = true;
+                    $ret->msg       = "E-mail enviado com sucesso! Em breve entraremos em contato com você.";
+                }
+                
+                echo json_encode($ret);
+            }catch(Exception $e){
+                //Objeto de retorno
+                $ret            = new stdClass();
+                $ret->status    = false;
+                $ret->msg       = "Erro: " . $e->getMessage() . " - Arquivo: " . $e->getFile() . " - Linha: " . $e->getFile();
+                
+                echo json_encode($ret);
             }
         }
     }
